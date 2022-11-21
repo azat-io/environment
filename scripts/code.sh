13aea576cc8c83956bd230b26a3a0bab88e64e9f
Azat S <to@azat.io>
2022-11-21 23:29:07 +0300

    feat: add vs code extensions

---
+++ scripts/code.sh
@@ -0,0 +1,97 @@
+# Auto Rename Tag
+# Auto rename paired HTML/XML tag
+code --install-extension formulahendry.auto-rename-tag
+
+# Browserslist
+# Syntax highlight for browserslist files
+code --install-extension webben.browserslist
+
+# Docker
+# Makes it easy to create, manage, and debug containerized applications.
+code --install-extension ms-azuretools.vscode-docker
+
+# DotENV
+# Support for dotenv file syntax
+code --install-extension mikestead.dotenv
+
+# EditorConfig
+# EditorConfig Support for Visual Studio Code
+code --install-extension EditorConfig.EditorConfig
+
+# ESLint
+# Integrates ESLint JavaScript into VS Code
+code --install-extension dbaeumer.vscode-eslint
+
+# GitHub Actions
+# GitHub Actions workflows and runs for github.com hosted repositories in VS Code
+code --install-extension cschleiden.vscode-github-actions
+
+# Gruvbox Theme
+code --install-extension jdinhlife.gruvbox
+
+# Haskell
+# Syntax support for the Haskell programming language
+code --install-extension justusadam.language-haskell
+
+# Import Cost
+# Display import/require package size in the editor
+code --install-extension wix.vscode-import-cost
+
+# IntelliCode
+# AI-assisted development
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+
+# Markdown All in One
+# All you need to write Markdown (keyboard shortcuts, table of contents, auto preview and more)
+code --install-extension yzhang.markdown-all-in-one
+
+# Material Icon Theme
+# Material Design Icons for Visual Studio Code
+code --install-extension PKief.material-icon-theme
+
+# MDX
+# Language support for MDX
+code --install-extension unifiedjs.vscode-mdx
+
+# Nginx
+# Syntax highlighting for NGINX configuration files
+code --install-extension william-voyek.vscode-nginx
+
+# NPM Intellisense
+# Visual Studio Code plugin that autocompletes npm modules in import statements
+code --install-extension christian-kohler.npm-intellisense
+
+# Path Intellisense
+# Visual Studio Code plugin that autocompletes filenames
+code --install-extension christian-kohler.path-intellisense
+
+# PostCSS
+# PostCSS language support
+code --install-extension ricard.postcss
+
+# Prettier
+# Code formatter using prettier
+code --install-extension esbenp.prettier-vscode
+
+# Styled Components
+# Syntax highlighting for styled-components
+code --install-extension styled-components.vscode-styled-components
+
+# Stylelint
+# Official Stylelint extension for Visual Studio Code
+code --install-extension stylelint.vscode-stylelint
+
+# Svelte
+# Svelte language support for VS Code
+code --install-extension svelte.svelte-vscode
+
+# Spotify
+# Use Spotify inside vscode
+code --install-extension shyykoserhiy.vscode-spotify
+
+# Syntax Highlighting for yarn.lock files
+code --install-extension mariusschulz.yarn-lock-syntax
+
+# Vue Language Features
+# Language support for Vue 3
+code --install-extension Vue.volar
